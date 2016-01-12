4a6bb96328fa34e8d2d28240988cb20d3a923434
Balaji Dasari <BalajiDasari@TTL001498.thetrainline.com>
2016-01-12 19:23:18 +0000

    Revert "Fixing Build"
    This reverts commit 5ddf91b93710bdf9c026b87aa0bce021849a7537.

---
+++ InterviewCodeTest/RestaurantSearch/RestaurantSearch/IOC.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Trainline.CardApi.Auditing.Repository;
+
+namespace RestaurantSearch
+{
+    public class AuditingModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder
+                .Register(context => new AuditRepository(context.ResolveNamed<IConnectionFactory>("AuditingConnectionFactory")))
+                .As<IAuditRepository>();
+
+            builder
+                .RegisterType<SqlServerAuditConnectionFactory>()
+                .Named<IConnectionFactory>("AuditingConnectionFactory");
+
+        }
+    }
+}
